6dfeb15befdc72f01cdaa46ee5eccfdd67569663
KARLA GUADALUPE VEGA GARCIA <13170369@itculiacan.edu.mx>
2019-04-10 11:54:03 -0600

    WebWorkers

---
+++ demoWorker.js
@@ -0,0 +1,10 @@
+var i = 0;
+
+function cuenta(){
+	i=i+1;
+	//Manda el mensaje a la pagina
+	postMessage(i);
+	//Cada 500 milisegundos ejecuta de nuevo cuenta()
+	setTimeout("cuenta()",500);
+}
+cuenta();
