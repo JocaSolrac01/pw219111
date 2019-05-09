bfb5ace096ca7f28827170f6bb9330aadccfd200
KARLA GUADALUPE VEGA GARCIA <13170369@itculiacan.edu.mx>
2019-05-09 11:54:24 -0600

    VUE_ejemplo completo

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -8,6 +8,28 @@ const app = new Vue({
 			{nombre:'pera', cantidad: 0},
 			{nombre:'platano', cantidad: 20},
 			{nombre:'mango', cantidad: 80}
-		]
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods: {
+		agregarFruta(){
+			// console.log("diste clic");
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad: 0
+			});
+			this.nuevaFruta = '';
+		}
+	},
+	computed: {
+		sumarFrutas(){
+			this.total = 0;
+			for(fruta of this.frutas2){
+				this.total = this.total + fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
 });
